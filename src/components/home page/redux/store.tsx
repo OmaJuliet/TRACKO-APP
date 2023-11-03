5ed07ec03b41472773e8f30628407045081a8c7f
Juliet Oma <julieto2001@gmail.com>
2023-11-03 09:58:40 +0100

    fixing deployment issue: localStorage

--- src/components/home page/redux/store.tsx
+++ src/components/home page/redux/store.tsx
@@ -1,24 +1,48 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import taskReducer, { loadTasks } from './taskSlice';
+
+// const store = configureStore({
+//   reducer: {
+//     tasks: taskReducer,
+//   },
+// });
+
+// const storedTasks = localStorage.getItem('tasks');
+// if (storedTasks) {
+//   const parsedTasks = JSON.parse(storedTasks);
+//   store.dispatch(loadTasks(parsedTasks)); 
+// }
+
+// store.subscribe(() => {
+//   const state = store.getState();
+//   localStorage.setItem('tasks', JSON.stringify(state.tasks.tasks));
+// });
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export default store;
+
+
 import { configureStore } from '@reduxjs/toolkit';
 import taskReducer, { loadTasks } from './taskSlice';
-// import appReducer from './appSlice';
 
 const store = configureStore({
   reducer: {
     tasks: taskReducer,
-    // app: appReducer,
   },
 });
 
-const storedTasks = localStorage.getItem('tasks');
-if (storedTasks) {
-  const parsedTasks = JSON.parse(storedTasks);
-  store.dispatch(loadTasks(parsedTasks)); 
-}
+if (typeof window !== 'undefined') {
+  const storedTasks = localStorage.getItem('tasks');
+  if (storedTasks) {
+    const parsedTasks = JSON.parse(storedTasks);
+    store.dispatch(loadTasks(parsedTasks));
+  }
 
-store.subscribe(() => {
-  const state = store.getState();
-  localStorage.setItem('tasks', JSON.stringify(state.tasks.tasks));
-});
+  store.subscribe(() => {
+    const state = store.getState();
+    localStorage.setItem('tasks', JSON.stringify(state.tasks.tasks));
+  });
+}
 
 export type RootState = ReturnType<typeof store.getState>;
 export default store;
